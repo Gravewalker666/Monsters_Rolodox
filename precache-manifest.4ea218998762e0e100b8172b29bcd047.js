db323d6cec6015cb949d95cceca4c71bfeb8cb9b
gravewalker666 <heshan.andris.pro@gmail.com>
2020-05-25 18:14:29 +0530

    Updates

---
+++ precache-manifest.4ea218998762e0e100b8172b29bcd047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a1216c26e51b7f1956085f0dcafc08",
+    "url": "/Monsters_Rolodox/index.html"
+  },
+  {
+    "revision": "cc6fd09fb21bca090449",
+    "url": "/Monsters_Rolodox/static/css/main.dfd8cb02.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/Monsters_Rolodox/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Rolodox/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6fd09fb21bca090449",
+    "url": "/Monsters_Rolodox/static/js/main.319eaf98.chunk.js"
+  },
+  {
+    "revision": "25165f1137a91b949c8f",
+    "url": "/Monsters_Rolodox/static/js/runtime-main.12d02567.js"
+  }
+]);
